ec9972550d83333e3d6a74bf2e721ec9407a87b1
Dmitriy <dima.ivahnoff@mail.ru>
2021-12-01 09:53:38 +0300

    draft__all-pages

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,22 @@
 (function ($) {
 	$(document).ready(function () {
-
-
+		var swiper = new Swiper(".mySwiper", {
+			loop: false,
+			spaceBetween: 30,
+			slidesPerView: 4,
+			grabCursor: true,
+		});
+		$('input').change(function() {
+			if ($(this).val())
+				$(this).addClass('has_value');
+			else
+				$(this).removeClass('has_value');
+		});
+		$('textarea').change(function() {
+			if ($(this).val())
+				$(this).addClass('has_value');
+			else
+				$(this).removeClass('has_value');
+		});
 	});
 })(jQuery);
